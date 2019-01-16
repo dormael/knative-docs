23eef4eb0d0e149665ac9d4e8b9ef6b50113aaef
dormael <dormael@gmail.com>
2019-01-16 14:56:10 +0000

    Add option for pandoc

--- build.pandoc.sh
+++ build.pandoc.sh
@@ -4,5 +4,5 @@
 
 FILES=( `go run pkg/main/main.go | sed 's/\/home\/dormael\/go\/src\/github.com\/dormael\/knative-docs\///' | sed 's/LocalFile //' | sed 's/^TOC.md$/README.md/'` )
 
-pandoc --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]}
-#pandoc --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]} && mv knative-doc.pdf /data0/download/
+#pandoc --pdf-engine=xelatex --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]}
+pandoc --pdf-engine=xelatex --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]} && mv knative-doc.pdf /data0/download/
