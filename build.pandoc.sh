563e80159c42b9f28d0fe33120eccd74ecb8d09d
Goohyun Chung <goohyun.chung@navercorp.com>
2019-01-17 11:29:34 +0900

    Added install component comment for pandoc 2.5

--- build.pandoc.sh
+++ build.pandoc.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#sudo apt install texlive-fonts-recommended texlive-latex-recommended
+#sudo apt install texlive-xetex librsvg2-bin
 
 FILES=( `go run pkg/main/main.go | sed 's/\/home\/dormael\/go\/src\/github.com\/dormael\/knative-docs\///' | sed 's/LocalFile //' | sed 's/^TOC.md$/README.md/'` )
 
 RESOURCE_PATH=".:./install:./build:./serving:./eventing:./serving/samples/autoscale-go:./serving/samples/knative-routing-go"
 
-#pandoc --pdf-engine=xelatex --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]}
 pandoc --resource-path=${RESOURCE_PATH} --pdf-engine=xelatex --toc --top-level-division=chapter -o knative-doc.pdf ${FILES[@]}
 #pandoc --resource-path=${RESOURCE_PATH} --css=pandoc.css --pdf-engine=xelatex --toc --top-level-division=chapter -o knative-doc.pdf install/Knative-with-Gardener.md
